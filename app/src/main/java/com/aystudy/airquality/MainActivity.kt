e676bad52acd9d22cc25cfc51dee9c734c00f26d
ayoung-jeon <wjsdkdud71@naver.com>
2026-04-15 17:14:22 +0900

    GPS설정 기능 추가

--- app/src/main/java/com/aystudy/airquality/MainActivity.kt
+++ app/src/main/java/com/aystudy/airquality/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.aystudy.airquality
 
 import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -18,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Applier
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -102,6 +108,41 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    private fun showDialogForLocationServiceSetting() {
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (isLocationServicesAvailable()) {
+                    isRunTimePermissionsGranted()
+                } else {
+                    Toast.makeText(this@MainActivity,
+                        "위치 서비스를 사용할 수 없습니다.",
+                        Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
+
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 꺼져있습니다. 설정해야 앱을 사용할 수 있습니다.")
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialogInterface, i ->
+            val callGPSSettingInterface = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingInterface)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener{ dialogInterface, i ->
+            dialogInterface.cancel()
+            Toast.makeText(this@MainActivity,
+                "위치 서비스를 사용할 수 없습니다.",
+                Toast.LENGTH_LONG).show()
+            finish()
+        })
+
+        builder.create().show()
     }
 }
 

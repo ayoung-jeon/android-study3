b1400552e9b4ae824c427505b8e9f2a148fd7d41
ayoung-jeon <wjsdkdud71@naver.com>
2026-04-15 15:59:29 +0900

    위치권한허용 기능 추가

--- app/src/main/java/com/aystudy/airquality/MainActivity.kt
+++ app/src/main/java/com/aystudy/airquality/MainActivity.kt
@@ -1,47 +1,107 @@
 package com.aystudy.airquality
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Applier
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.aystudy.airquality.databinding.ActivityMainBinding
 import com.aystudy.airquality.ui.theme.AirQualityTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    val REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    lateinit var getGPSPermissionLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AirQualityTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkAllPermissions()
+    }
+
+    private fun checkAllPermissions() {
+        if (!isLocationServicesAvailable()) {
+            showDialogForLocationServiceSetting()
+        } else {
+            isRunTimePermissionsGranted()
+        }
+    }
+
+    private fun isLocationServicesAvailable() : Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+    }
+
+    private fun isRunTimePermissionsGranted() {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this@MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        if (hasFineLocationPermission !=PackageManager.PERMISSION_GRANTED
+            || hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@MainActivity, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE &&
+            grantResults.size == REQUIRED_PERMISSIONS.size
+        ) {
+            var checkResult = true
+
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    checkResult = false
+                    break
                 }
+            }
+
+            if (checkResult) {
+                // 위치값을 가지고 올 수 있음
+            } else {
+                Toast.makeText(this@MainActivity,
+                    "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                    Toast.LENGTH_LONG).show()
+                finish()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AirQualityTheme {
-        Greeting("Android")
-    }
-}

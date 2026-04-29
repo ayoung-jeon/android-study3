29b9b38f38ac0f67e6484a9767afefa33a5aa5fa
ayoung-jeon <wjsdkdud71@naver.com>
2026-04-29 10:00:11 +0900

    레이아웃 설정1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.appcompat)
+    // Material Components - XML View용 FAB
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

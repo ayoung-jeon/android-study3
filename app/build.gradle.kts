32f255955f219d9855da3bcf10b04782c9956c4d
ayoung-jeon <wjsdkdud71@naver.com>
2026-04-29 11:18:11 +0900

    레이아웃 설정2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     // Material Components - XML View용 FAB
     implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

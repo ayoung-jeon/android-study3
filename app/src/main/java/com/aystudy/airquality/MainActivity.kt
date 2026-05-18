b81e4ef7d025626210e66f1acb9ede8486fb415f
ayoung-jeon <wjsdkdud71@naver.com>
2026-05-18 10:25:59 +0900

    플로팅 액션 버튼 추가

--- app/src/main/java/com/aystudy/airquality/MainActivity.kt
+++ app/src/main/java/com/aystudy/airquality/MainActivity.kt
@@ -14,8 +14,11 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
@@ -52,12 +55,27 @@ class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
 
+    var latitude : Double? = 0.0
+    var longitude : Double? = 0.0
+
     val REQUIRED_PERMISSIONS = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
 
     lateinit var getGPSPermissionLauncher: ActivityResultLauncher<Intent>
+
+    val startActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+        object : ActivityResultCallback<ActivityResult> {
+            override fun onActivityResult(result: ActivityResult) {
+                if (result?.resultCode?: 0 == Activity.RESULT_OK) {
+                    latitude = result?.data?.getDoubleExtra("latitude", 0.0) ?: 0.0
+                    longitude = result?.data?.getDoubleExtra("longitude", 0.0) ?: 0.0
+                    updateUI()
+                }
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,17 +85,21 @@ class MainActivity : AppCompatActivity() {
         checkAllPermissions()
         updateUI()
         setRefreshButton()
+
+        setFab()
     }
 
     private fun updateUI() {
         locationProvider = LocationProvider(this@MainActivity)
 
-        val latitude : Double? = locationProvider.getLocationLatitude()
-        val longitude : Double? = locationProvider.getLocationLongitude()
+        if(latitude == 0.0 && longitude == 0.0) {
+            latitude = locationProvider.getLocationLatitude()
+            longitude = locationProvider.getLocationLongitude()
+        }
 
         if (latitude != null && longitude != null) {
             // 1. 현재 위치 가져오고 UI 업데이트
-            val address = getCurrentAddress(latitude, longitude)
+            val address = getCurrentAddress(latitude!!, longitude!!)
 
             address?.let {
                 binding.tvLocationTitle.text = "${it.thoroughfare}"
@@ -85,7 +107,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             // 2. 미세먼지 농도 가져오고 UI 업데이트
-            getAirQualityData(latitude, longitude)
+            getAirQualityData(latitude!!, longitude!!)
 
         } else {
             Toast.makeText(this, "위도, 경도 정보를 가져올 수 없습니다.", Toast.LENGTH_LONG).show()
@@ -168,6 +190,15 @@ class MainActivity : AppCompatActivity() {
     private fun setRefreshButton() {
         binding.btnRefresh.setOnClickListener {
             updateUI()
+        }
+    }
+
+    private fun setFab() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            intent.putExtra("currentLat", latitude)
+            intent.putExtra("currentLon", longitude)
+            startActivityForResult.launch(intent)
         }
     }
 
